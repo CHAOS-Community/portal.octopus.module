94649af1e8844fea8ae5674ece2186a5ff06c25c
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2014-01-29 01:57:47 +0100

    Added JobDto parameter binding for json formattet input job data

---
+++ src/app/Chaos.Octopus/Module/Bindings/JobParameterBinding.cs
@@ -0,0 +1,20 @@
+﻿namespace Chaos.Octopus.Module.Bindings
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Extension.Dto;
+    using Portal.Core.Bindings;
+
+    public class JobParameterBinding : IParameterBinding
+    {
+        public object Bind(IDictionary<string, string> parameters, ParameterInfo parameterInfo)
+        {
+            var json = parameters["data"];
+
+            var job = Newtonsoft.Json.JsonConvert.DeserializeObject<Job>(json);
+            job.Data = json;
+
+            return job;
+        }
+    }
+}
---
+++ src/test/Chaos.Octopus.Test/Module/Bindings/JobParameterBindingTest.cs
@@ -0,0 +1,39 @@
+﻿namespace Chaos.Octopus.Test.Module.Bindings
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using Octopus.Module.Bindings;
+    using Octopus.Module.Extension.Dto;
+
+    [TestFixture]
+    public class JobParameterBindingTest
+    {
+         [Test]
+         public void Bing_WithJobJson_ReturnJobDto()
+         {
+             var json = Make_JobJson();
+             var binding = new JobParameterBinding();
+             var parameters = new Dictionary<string, string> {{"data", json}};
+             var parameter = typeof (MyClass).GetMethod("Test").GetParameters().First();
+                
+             var result = (Job) binding.Bind(parameters, parameter);
+
+             Assert.That(result.Id, Is.EqualTo("0123456789"));
+             Assert.That(result.Data, Is.EqualTo(json));
+         }
+
+         private string Make_JobJson()
+         {
+             return "{\"id\":\"0123456789\", \"steps\":[{\"tasks\":[{\"pluginId\":\"com.chaos.octopus.agent.unit.TestPlugin, 1.0.0\",\"properties\":{\"sleep\":\"3000\",\"number\":\"2\"}}]}]}";
+         }
+
+        class MyClass
+        {
+            public void Test(Job job)
+            {
+                
+            }
+        }
+    }
+}
